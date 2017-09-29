981d17fc9b5a35c146ce4f0e0593bf5a052c24fd
leslie <laszlo.kocsonya@vcc.live>
2017-09-29 09:54:55 +0200

    Move file

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import { temperatureReader } from './temperatureReader';
+import { temperatureReader } from './lib/temperatureReader';
 
 require('dotenv').config();
 
