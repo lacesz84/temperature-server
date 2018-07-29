96c16ef8a3793e52cb20d054e6c2ff646ce0d1c8
Kocsonya László <laszlo.kocsonya@vcc.live>
2018-07-29 16:40:58 +0200

    Ignore dist/

--- dist/src/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const temperatureServer_1 = require("./lib/temperatureServer");
-require('dotenv').config();
-const server = new temperatureServer_1.TemperatureServer();
-server.start();
-//# sourceMappingURL=app.js.map
